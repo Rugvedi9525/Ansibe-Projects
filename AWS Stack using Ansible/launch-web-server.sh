c89bf41a422bb5a36df65d5547f5c3f8baf97bec
Rugvedi Kapse <rugvedi25@gmail.com>
2020-01-26 20:51:48 -0500

    Add files via upload

---
+++ AWS Stack using Ansible/launch-web-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$1 = access_key
+$2 = secret_key
+$3 = my_keys 
+ansible-playbook -i inventory.txt launch-web-server.yaml -e "access_key=$access_key secret_key=$secret_key my_keys=$my_keys"
